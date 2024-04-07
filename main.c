ed8b1a5de1c05aed7897ae15428276370c4c28bc
Ryan Cole <colrryan110@gmail.com>
2024-04-06 20:35:57 -0500

    Implement Password Enter Function
    Note: The system password is 123456

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
+#include <conio.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 // Data structures
+
+#define Correct_Password "123456"
 
 typedef enum { E10_87, E10_90, Diesel } FuelType;
 
@@ -35,7 +40,7 @@ void generateReceipt();
 void blankCharge();
 void addCharge();
 void existingCharge();
-void enterPassword();
+bool enterPassword(); // Returns true if password is valid
 void updateCharge();
 void deleteCharge();
 void chargeSearch();
@@ -43,4 +48,35 @@ void chargePayment();
 void refuelTank();
 void generateReport();
 
-int main() { return 0; }
+int main() {
+  enterPassword();
+  return 0;
+}
+
+bool enterPassword() {
+  char password[100] = {0};
+
+  printf("Authorization Required\n");
+  printf("Enter Password:");
+
+  int i = 0;
+  char c;
+
+  // User enters the password
+  do {
+    c = getch(); // Record each charcter
+    if (c == 13)
+      break;
+    sprintf(password, "%s%c", password, c);
+    printf("*"); // Print a mask instead of the character
+  } while (c != EOF);
+  printf("\n");
+
+  if (strcmp(password, Correct_Password) == 0) {
+    printf("Access Granted\n");
+    return true;
+  } else {
+    printf("Invalid Password Entered\n");
+    return false;
+  }
+}
