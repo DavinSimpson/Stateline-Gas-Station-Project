180f06f3398b862f2d021911ff869a8fa09bb7ba
Matt-heyo <matthew.webster0820@gmail.com>
2024-04-10 09:51:56 -0500

    Update main.c
    Added Main Menu functionality with some minor comments

--- main.c
+++ main.c
@@ -80,3 +80,62 @@ bool enterPassword() {
     return false;
   }
 }
+
+void mainMenu() {
+    // Declare variables for menu use
+    int choice;
+   
+
+    // Main menu loop
+    do {
+        // Display menu options
+        printf("\nMain Menu\n");
+        printf("1. Serve Customer\n");
+        printf("2. Add Charge Customer\n");
+        printf("3. Update Charge Customer\n");
+        printf("4. Delete Charge Customer\n");
+        printf("5. Make Payment to Charge Account\n");
+        printf("6. Refuel Tank\n");
+        printf("7. Generate Report\n");
+        printf("8. Search for existing Charge Customer \n");
+
+        printf("9. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        // Execute corresponding function based on user choice
+        switch (choice) {
+            case 1:
+                serveCustomer();
+                break;
+            case 2:
+                addCharge();
+                break;
+            case 3:
+                updateCharge();
+                break;
+            case 4:
+                deleteCharge();
+                break;
+            case 5:
+                chargePayment();
+                break;
+            case 6:
+                refuelTank();
+                break;
+            case 7:
+                generateReport();
+                break;
+            case 8:
+                chargeSearch();
+                break;
+            case 9:
+                exit();
+            
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 9);
+
+    return 0;
+}
